451028eb9cfa26a77f77b53cb97737a04cf38e82
rishika <24wh1a05u2@bvrithyderabad.edu.in>
2026-07-04 09:57:39 +0530

    second commit

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,57 @@
 console.log("Hello from rishika");
 alert("Hello from rishika");
+
+const users = [
+  {
+    "name" :"Jane Doe",
+    "gender" : "Female",
+    "image" : "jane.png"
+  },
+  {
+    "name" :"John Doe",
+    "gender" : "Male",
+    "image" : "john.png"
+  },
+]
+let curUserId = 0;
+function toggle(){
+   // console.log("frontend");
+   // gets printed inside the browser console, not in the terminal
+  if(curUserId == 0)
+    curUserId=1;
+  else
+    curUserId=0;
+  // image
+  document.getElementById("img").src=users[curUserId].image;
+  // name
+  document.getElementById("card-name").innerText=users[curUserId].name;
+  document.getElementById("card-gender").innerText=users[curUserId].gender;
+  
+}
+
+console.log("Hello from JS");
+function getRandomUser(){
+    fetch("https://randomuser.me/api/")
+    .then(function(data){
+        return data.json();
+    })
+    .then(function(parsedData){
+       // console.log(parsedData);
+       let gender= parsedData.results[0].gender;
+       let first=parsedData.results[0].name.first;
+       let last=parsedData.results[0].name.last;
+       let name= first+" "+last;
+       //console.log(name);
+      // let name=;
+       let imageUrl=parsedData.results[0].picture.large;
+       //gender
+       document.getElementById("card-gender").innerText=gender;
+       //name
+       document.getElementById("card-name").innerText=name;
+        //img
+        document.getElementById("img").src=imageUrl;
+
+
+
+    })
+}
